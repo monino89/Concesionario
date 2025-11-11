635b3115520e0c69fc6cf7f7b4b4c60aaff06117
CENTIC <CENTIC@10.6.65.242>
2025-11-11 11:48:56 -0500

    Inicio de menu

--- src/Utility/Menu.java
+++ src/Utility/Menu.java
@@ -1,7 +1,19 @@
 //menu de la aplicacion
 package Utility;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Menu {
     public static void printMenu(){
-        System.out.println("\n\tAVAILABLE OPTIONS\n\n1. View vehicle information\n2. view ");
+        System.out.println("\n\nAVAILABLE OPTIONS");
+        System.out.println("1. Print vehicles information");
+        System.out.println("2. Print clients information");
+        System.out.println("3. Modify an existing Vehicle");
+        System.out.println("4. ");
     }
 }
